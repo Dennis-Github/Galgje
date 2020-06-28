a1454b282b51b8597159aa09f139f9614d83de20
Dennis Schroder <110062@st-maartenscollege.nl>
2020-06-28 11:48:17 +0000

    Added english words to guess from

---
+++ main.py
@@ -0,0 +1,15 @@
+import random
+from words import words
+
+
+
+def get_valid_word(words):
+    word = random.choice(words)  # randomly chooses word 
+    while '-' in word or ' ' in word:
+        word = random.choice(words)
+
+    return word.upper()
+
+
+
+    
